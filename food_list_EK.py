dc1e56539c5889320b6e44a2fc60c976c3501257
EHDEK <144786195+EdKow@users.noreply.github.com>
2023-12-04 16:10:35 +0700

    Update food_list_EK.py

--- food_list_EK.py
+++ food_list_EK.py
@@ -2,34 +2,39 @@ class food():
     def __init__(self, foodname, pounds):
         self.__foodname = foodname
         self.__weight = pounds
-        self.__standardprice = 0
-        self.__calcprice = 0
+        self.__standpriceEK()
+        self.__calcpriceEK()
 
-    def __standprice(self, foodname, price):
-        self.__standardprice = price
-        if foodname == "Dry Cured Iberian Ham":
-            price = 177.80
-        elif foodname == "Wagyu Steaks":
-            price = 450.00
-        elif foodname == "Matsutake Mushrooms":
-            price = 272.00
-        elif foodname == "Kopi Luwak Coffee":
-            price = 306.5
-        elif foodname == "Moose Cheese":
-            price = 487.20
-        elif foodname == "White Truffles":
-            price = 3600.00
-        elif foodname == "Blue Fin Tuna":
-            price = 3603.00
-        elif foodname == "Le Bonnotte Potatoes":
-            price = 270.81
-        else:
-            price:0.00
+    def GetnameEK(self):
+        return self.__foodname
+    def GetweightEK(self):
+        return self.__weight
+    def GetcalcpriceEK(self):
+        return self.__calcedprice
+    def GetstandardpriceEK(self):
+        return self.__standardprice
+    def __standpriceEK(self):
+        foodname = self.__foodname.lower()
         
-    def __calcprice(self):
-        tot = self.__weight*self.__standardprice
-        return tot
-    
-
-    def __str__(self):
-        return (f"")
+        if foodname == "dry cured iberian ham":
+            self.__standardprice = 177.80
+        elif foodname == "wagyu steaks":
+            self.__standardprice = 450.00
+        elif foodname == "matsutake mushrooms":
+            self.__standardprice = 272.00
+        elif foodname == "kopi luwak coffee":
+            self.__standardprice = 306.5
+        elif foodname == "moose cheese":
+            self.__standardprice = 487.20
+        elif foodname == "white truffles":
+            self.__standardprice = 3600.00
+        elif foodname == "blue fin tuna":
+            self.__standardprice = 3603.00
+        elif foodname == "le bonnotte potatoes":
+            self.__standardprice = 270.81
+        else:
+            self.__standardprice:0.00
+        
+    def __calcpriceEK(self):
+        self.__calcedprice = self.__weight*self.__standardprice
+        return self.__calcedprice
